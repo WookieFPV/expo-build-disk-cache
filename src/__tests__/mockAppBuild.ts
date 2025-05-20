9e4f4ab812ae41cf566873fd2c6a341ee2e03193
Lukas Müller <WookieFPV@users.noreply.github.com>
2025-05-20 23:43:09 +0200

    chore: improve tests

---
+++ src/__tests__/mockAppBuild.ts
@@ -0,0 +1,11 @@
+import path from "node:path";
+import { file } from "bun";
+import env from "env-paths";
+
+export const mockAppBuild = async (fingerprintHash: string) => {
+	const tmpDir = env("expo-build-disk-cache", { suffix: "tests" }).temp;
+	const appFile = file(path.join(tmpDir, `${fingerprintHash}.apk`));
+	await appFile.write("Placeholder for real apk file");
+	if (!appFile.name) throw new Error("mockBuild failed");
+	return appFile.name;
+};
